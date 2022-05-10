24c5546047e9d56fee881ab691d2306524e5055f
def750 <def75023@gmail.com>
2022-05-10 07:55:55 +0000

    Update Structure 2

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from __future__ import annotations
 
+from . import routes
+from . import const
 from . import utils
---
+++ routes/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from . import common
+from . import settings
+from . import auth
+from . import profile
+from . import admin
