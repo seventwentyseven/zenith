2ba3fb12aeb5678153e9d3e21a31a5eb5f0c1bd4
def750 <def75023@gmail.com>
2022-05-11 20:16:16 +0000

    Few Updates - Update gitignore - Add flash to common routes - Add Footer back - Rework base.html - Add navbar back - Made navbar sticky - Added postcss to package.json

--- routes/common.py
+++ routes/common.py
@@ -7,6 +7,7 @@ from quart import render_template
 from quart import request
 from quart import send_file
 from quart import session
+from quart import flash
 
 import app.state
 import zenith.config
