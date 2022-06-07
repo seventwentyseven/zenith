9bc134a27d9ed5dc3ed9d75e852c8256d009adab
def750 <def75023@gmail.com>
2022-06-07 11:25:47 +0000

    Flashes support + shit ton of bugfixes + custom scrollbar

--- routes/common.py
+++ routes/common.py
@@ -8,6 +8,8 @@ from quart import request
 from quart import send_file
 from quart import session
 from quart import flash
+from quart import url_for
+from quart import get_flashed_messages
 
 import app.state
 import zenith.config
@@ -15,8 +17,29 @@ import zenith.config
 common = Blueprint('common', __name__)
 
 @common.route('/', methods=['GET'])
-async def index():
+async def home():
     if 'authenticated' in session:
         return await render_template('/pages/common/home.html')
     else:
         return await render_template('/pages/common/home_guest.html')
+
+@common.route('/team', methods=['GET'])
+async def team():
+    return await render_template('/pages/common/team.html')
+
+@common.route('/login', methods=['POST'])
+async def login():
+    # Login is by login modal, get the form data and check if it's valid
+    if 'authenticated' in session:
+        flash('You are already logged in.', 'warning')
+        return redirect(url_for('index'))
+    form = await request.form
+    print(form)
+    # Flash errors if the form is invalid
+    await flash('Please fill in all fields.', 'error')
+    await flash('Please fill in all fields.', 'warning')
+    await flash('Please fill in all fields.', 'success')
+    await flash('Please fill in all fields.', 'error')
+    await flash('Please fill in all fields.', 'warning')
+    await flash('Please fill in all fields.', 'success')
+    return redirect(url_for('common.home'))
