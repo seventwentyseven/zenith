524e4e9705475d2e4c52ef4f22a02e80d185d26a
def750 <def75023@gmail.com>
2022-10-01 09:25:06 +0000

    Backend logic for leaderboards and donate page

--- routes/api/commonapi.py
+++ routes/api/commonapi.py
@@ -1,4 +1,7 @@
 import datetime as dt
+import math
+import os
+from pathlib import Path
 
 from quart import Blueprint
 from quart import request
@@ -7,30 +10,202 @@ from quart import session
 
 import app.state
 import app.settings
+from app.constants.gamemodes import GameMode
+from app.constants.privileges import Privileges
 
 
 commonapi = Blueprint('commonapi', __name__)
 
+BANNERS_PATH = Path.cwd() / '.data/banners'
+
 @commonapi.route('/', methods=['GET'])
 async def index():
+    return ":D"
     print(app.state.web.cache)
     return {"the": "j"}
 
 
 @commonapi.route('/home/', methods=['GET'])
 async def home():
+    return ":D"
     return {
-            "success": True,
-            "articles": Cache.get('articles'),
-            "recent_ranked": Cache.get('recent_ranked'),
-            "most_played_24h": Cache.get('most_played_24h'),
+        "success": True,
+        "articles": Cache.get('articles'),
+        "recent_ranked": Cache.get('recent_ranked'),
+        "most_played_24h": Cache.get('most_played_24h'),
     }, 200
 
 # Route for getting point data for online users graph (last 24h)
+
+
 @commonapi.route('/online_data/', methods=['GET'])
 async def online_data():
+    return ":D"
     return {
-            "success": True,
-            "data": Cache.get('online_data'),
+        "success": True,
+        "data": Cache.get('online_data'),
     }, 200
 
+
+@commonapi.route('/leaderboard', methods=['GET'])
+async def leaderboard():
+    # Get arguments
+    mode = request.args.get('mode', type=int, default=0)
+    mods = request.args.get('mods', type=str, default='vn')
+    page = request.args.get('page', type=int, default=1)
+    country = request.args.get('country', type=str, default=None)
+    _type = request.args.get('type', type=str, default='pp')
+
+    # Validate args
+    if mode not in range(0, 4):
+        mode = 0
+
+    if _type not in ('pp', 'plays', 'rscore'):
+        _type = 'pp'
+
+    if country is not None and country not in app.state.services.country_codes.keys():
+        country = None  # None = Global lb
+
+    if mods not in ('vn', 'rx', 'ap'):
+        mods = 'vn'
+
+    # Convert mods
+    q_mode = GameMode.gulag_int_conv(mode, mods)
+    # Get information abaout max page with current filters
+    res1 = await app.state.services.database.fetch_val(
+        "SELECT COUNT(*) FROM users u "
+        "LEFT JOIN stats s ON u.id=s.id "
+        "WHERE u.priv & 1 AND s.pp > 0 AND s.mode = :mode "
+        "AND (u.country = :country OR :country IS NULL)",
+        {'mode': q_mode, 'country': country}
+    )
+
+    # Calculate max page
+    max_page = math.ceil(res1 / 50)
+    del res1  # Not needed anymore
+
+    # Page checking
+    if page < 1:
+        page = 1
+    elif page > max_page:
+        page = max_page if max_page > 0 else 1
+
+    sort = f"s.{_type}"
+    # Get actual leaderboard data
+    lb = await app.state.services.database.fetch_all(
+        "SELECT u.id, u.name, u.country, s.pp, "
+        "s.acc, s.max_combo, s.plays, c.tag, u.clan_id "
+        "FROM users u "
+        "LEFT JOIN stats s ON u.id=s.id "
+        "LEFT JOIN clans c ON u.clan_id=c.id "
+        "WHERE u.priv & 1 AND s.pp > 0 AND s.mode = :mode "
+        "AND (u.country = :country OR :country IS NULL) "
+        f"ORDER BY {sort} DESC LIMIT 50 OFFSET :offset",
+        {'mode': q_mode, 'country': country, 'offset': (page-1)*50}
+    )
+    # Convert to dict
+    lb = [dict(user) for user in lb]
+
+    return {
+        "success": True,
+        "data": lb,
+        'args': {
+            'mode': mode,
+            'mods': mods,
+            'country': country,
+            'page': page,
+            'type': _type,
+        },
+        'max_page': max_page,
+        'gulag_mode': q_mode,
+    }, 200
+
+
+@commonapi.route('/no1lb', methods=['GET'])
+async def no1lb():
+    # Get arguments
+    mode = request.args.get('mode', type=int, default=0)
+    mods = request.args.get('mods', type=str, default='vn')
+    country = request.args.get('country', type=str, default=None)
+    _type = request.args.get('type', type=str, default='pp')
+
+    # Validate args
+    if mode not in range(0, 4):
+        mode = 0
+
+    if mods not in ('vn', 'rx', 'ap'):
+        mods = 'vn'
+
+    if _type not in ('pp', 'acc', 'plays'):
+        _type = 'pp'
+
+    if country is not None and country not in app.state.services.country_codes.keys():
+        country = None  # None = Global lb
+
+    mode = GameMode.gulag_int_conv(mode, mods)
+    sort = f"s.{_type}"
+
+    res = await app.state.services.database.fetch_val(
+        "SELECT u.id "
+        "FROM users u LEFT JOIN stats s ON u.id=s.id "
+        "WHERE u.priv & 1 AND s.mode = :mode "
+        "AND (u.country = :country OR :country IS NULL) "
+        f"ORDER BY {sort} DESC LIMIT 1",
+        {'mode': mode, 'country': country}
+    )
+    print(res)
+    for ext in ('gif', 'jpg', 'png', 'jpeg'):
+        if os.path.exists(BANNERS_PATH / f'{res}.{ext}'):
+            imageExists = True
+            break
+    else:
+        imageExists = False
+
+    # Check if image exists at /banners
+    return {"success": True, "userid": res, 'image': imageExists}, 200
+
+@commonapi.route('/card_data', methods=['GET'])
+async def carddata():
+    #NOTE: No badges here, they should be implemented user side :trollface:
+    #      Work them out using priv value
+
+    # Get arguments
+    name = request.args.get('name', type=str, default=None)
+    uid = request.args.get('uid', type=int, default=None)
+
+    # One must be present
+    if name is None and uid is None:
+        return {"success": False, "error": "You must specify uid or name argument"}, 400
+
+    # Get user
+    user = await app.state.services.database.fetch_one(
+        "SELECT u.id, u.name, u.priv, u.country, "
+        "u.latest_activity, c.tag "
+        "FROM users u LEFT JOIN clans c ON u.clan_id=c.id "
+        "WHERE u.name = :name OR u.id = :uid",
+        {'name': name, 'uid': uid}
+    )
+
+    if user is None:
+        return {"success": False, "error": "User not found"}, 200
+    else:
+        user = dict(user)
+        # TODO: Check if requester is staff, if so allow to see hidden users
+        # TODO2: Check if user has 'Privileges.HIDDEN', if so also return not found
+        if not user['priv'] & Privileges.UNRESTRICTED:
+            return {"success": False, "error": "User not found"}, 200
+
+        # Get user online status from app.state.players
+        p = await app.state.sessions.players.from_cache_or_sql(id=user['id'])
+        user['online'] = p.online if p is not None else False
+
+    # Determine if user has banner
+    for ext in ('gif', 'jpg', 'png', 'jpeg'):
+        if os.path.exists(BANNERS_PATH / f'{user["id"]}.{ext}'):
+            user['has_banner'] = True
+            break
+    else:
+        user['has_banner'] = False
+
+
+    return {'success': True, 'data': user}, 200
--- routes/common.py
+++ routes/common.py
@@ -1,7 +1,9 @@
+from typing import Type
 import bcrypt
 import datetime as dt
 import hashlib
 import os
+from pathlib import Path
 
 from quart import Blueprint
 from quart import redirect
@@ -20,6 +22,9 @@ import app.state
 import app.settings
 
 common = Blueprint('common', __name__)
+
+BANNERS_PATH = Path.cwd() / '.data/banners'
+
 
 @common.route('/', methods=['GET'])
 async def home():
@@ -40,9 +45,11 @@ async def home():
     else:
         return await render_template('/pages/common/home_guest.html', data=data)
 
+
 @common.route('/team', methods=['GET'])
 async def team():
     return await render_template('/pages/common/team.html')
+
 
 @common.route('/docs', methods=['GET'])
 async def docs():
@@ -55,10 +62,12 @@ async def docs():
 
     return await render_template('/pages/docs/index.html', data=data)
 
+
 @common.route('/docs/<name>', methods=['GET'])
 async def docs_page(name):
     content = open("zenith/templates/pages/docs/{}.html".format(name)).read()
     return content
+
 
 @common.route('/login', methods=['POST'])
 async def login():
@@ -69,7 +78,7 @@ async def login():
     il = app.state.web.invalid_logins
     user_ip = request.headers.get('CF-Connecting-IP')
     now = dt.datetime.utcnow()
-    #TODO: Redirect to viewed page
+    # TODO: Redirect to viewed page
 
     # First, check if user is already logged in
     if 'authenticated' in session:
@@ -80,14 +89,14 @@ async def login():
         await flash('Captcha failed or left unsolved, please try again.', 'warning')
         return redirect(url_for('common.home'))
 
-
     # Check if user has tried to login too many times
     # and if last try was less than 15 minutes ago
     if user_ip in il:
         if il[user_ip]['count'] >= 3:
             if now - il[user_ip]['last_try'] < dt.timedelta(minutes=15):
                 # Create time reaming var, return str `{x} minutes, {x} seconds`
-                time_reaming: dt.timedelta = dt.timedelta(minutes=15) - (now - il[user_ip]['last_try'])
+                time_reaming: dt.timedelta = dt.timedelta(
+                    minutes=15) - (now - il[user_ip]['last_try'])
                 await flash(
                     f'You have tried to login too many times. Please wait {time_reaming.seconds // 60} minutes.',
                     'error'
@@ -100,7 +109,8 @@ async def login():
 
     # Get user's encrypted password and check if user exists
     user = await app.state.services.database.fetch_one(
-        "SELECT id, name, priv, pw_bcrypt FROM users "
+        "SELECT id, name, priv, pw_bcrypt, preferred_mode, clan_id "
+        "FROM users "
         "WHERE name = :username",
         {'username': username}
     )
@@ -110,10 +120,8 @@ async def login():
     else:
         user = dict(user)
 
-
     # Check if the password is correct
-    print(pwd_valid := await validate_password(user['pw_bcrypt'], passwd_txt))
-    if not pwd_valid:
+    if not await validate_password(user['pw_bcrypt'], passwd_txt):
         # add ip into il, if doesn't exist create it
         if user_ip not in il:
             il[user_ip] = {'count': 1, 'last_try': now}
@@ -130,16 +138,24 @@ async def login():
 
         return redirect(url_for('common.home'))
     else:
-        #* Login successful
+        # * Login successful
         # Delete from invalid logins
         if user_ip in il:
             del il[user_ip]
+        del user['pw_bcrypt']
 
-        #* Login successful
+        # * Login successful
         await make_session(user)
-        await flash ('You are now logged in! Thank you for playing on 727 ❤️', 'success')
+        await flash('You are now logged in! Thank you for playing on 727 ❤️', 'success')
+        await app.state.services.database.execute(
+            "INSERT INTO website_logins (userid, ip, browser) "
+            "VALUES (:userid, :ip, :browser)",
+            {'userid': user['id'], 'ip': user_ip,
+                'browser': request.headers.get('User-Agent')}
+        )
         print("Total time: ", dt.datetime.utcnow() - now)
         return redirect(url_for('common.home'))
+
 
 @common.route('/logout', methods=['GET'])
 async def logout():
@@ -151,21 +167,73 @@ async def logout():
         await flash('You are not logged in.', 'warning')
     return redirect(url_for('common.home'))
 
+
 @common.route('/test')
 async def test():
     print(session)
     return 'Logs'
 
+
 @common.route('/login/2fa', methods=['GET', 'POST'])
 async def login_2fa():
-    #TODO: 2FA support, more info on trello
+    # TODO: 2FA support, more info on trello
     return {'status': 'ok'}
+
+#! grafika dzifors code
+
+
+@common.route('/scores/<scoreid>', methods=['GET'])
+async def scores(scoreid):
+    data = await app.state.services.database.fetch_one(
+        "SELECT * "
+        "FROM scores "
+        "WHERE id = :id",
+        {'id': scoreid}
+    )
+    if not data:
+        await flash('Score not found!', 'error')
+    else:
+        data = dict(data)
+    return await render_template('/pages/common/scores.html', scoreid=scoreid)
+
 
 @common.route('/leaderboard', methods=['GET'])
 async def leaderboard():
-    # Args
-    mode = request.args.get('mode', type=int, default=None)
-    page = request.args.get('page', type=int, default=1)
-    country = request.args.get('country', type=str, default=None)
-    _type = request.args.get('type', type=str, default='pp')
-    return await render_template('/pages/common/leaderboard.html')
+    # Render leaderboard page, all logic is done in JS and API
+    # Determine user's default mode
+    defmode = 0
+    if 'authenticated' in session:
+        defmode = session['user']['preferred_mode']
+
+    return await render_template('/pages/common/leaderboard.html', defmode=defmode)
+
+
+@common.route('/testcolors', methods=['GET'])
+async def testcolors():
+    return await render_template('/pages/common/testcolors.html')
+
+@common.route('/donate', methods=['GET'])
+async def donate():
+    if 'authenticated' not in session:
+        await flash('You must be logged in to access this page!', 'error')
+        return redirect(url_for('common.home'))
+    return await render_template('/pages/common/donate.html')
+@common.route('/donate/checkout', methods=['POST'])
+async def donate_checkout():
+    if 'authenticated' not in session:
+        await flash('You must be logged in to access this page!', 'error')
+        return redirect(url_for('common.home'))
+    # Get form data and print it
+    data = await request.form
+    print(data)
+    return redirect(url_for('common.donate'))
+
+
+@common.route('/banners/<uid>')
+async def get_banner(uid: int):
+    # Check if file exists
+    for ext in ('png', 'jpg', 'jpeg', 'gif'):
+        if os.path.exists(BANNERS_PATH / f'{uid}.{ext}'):
+            return await send_file(f'{BANNERS_PATH}/{uid}.{ext}')
+    else:
+        return {'success': False, 'message': 'This user does not have a banner.'}
