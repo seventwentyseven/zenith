08a29abf145e0ff6ee164df3b77b5fde074f5944
def750 <def75023@gmail.com>
2023-02-23 21:07:57 +0000

    Change dev badge styling

--- components/badges/badge-developer.tsx
+++ components/badges/badge-developer.tsx
@@ -2,9 +2,9 @@ import { MdCode } from 'react-icons/md'
 
 const BadgeDeveloper = () => {
   return (
-    <div className="flex flex-row items-center justify-center bg-purple-300 shadow-sm shadow-purple-500 rounded-lg py-4 px-3 h-8">
-      <MdCode className="mr-2" />
-      Dev
+    <div className="flex flex-row items-center justify-center bg-gradient-to-br from-purple-700 to-fuchsia-700 rounded-lg py-4 px-3 h-8">
+      <MdCode className="mr-2 text-rose-100 text-xl" />
+      <span className="text-rose-100">Dev</span>
     </div>
   )
 }
