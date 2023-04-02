46b43742b3c7572560f92264bd82f8927c50bf3d
def750 <def75023@gmail.com>
2023-04-02 18:45:10 +0000

    Delete old code

--- components/badges/BadgeWhitelisted.tsx
+++ components/badges/BadgeWhitelisted.tsx
@@ -1,22 +1,3 @@
-// const BadgeWhitelisted = () => {
-//   return (
-//     <>
-//       <span
-//         className="v inline-flex items-center rounded-full px-2.5 py-2 bg-gradient-to-br from-green-400 to-green-500 text-green-100 group transition-all duration-300"
-//         role="alert"
-//         tabIndex={0}
-//       >
-//         <FaUserCheck className="text-xl" />
-//         <span className="whitespace-nowrap inline-block group-hover:max-w-screen-2xl max-w-0 scale-80 group-hover:scale-100 overflow-hidden transition-all duration-300 group-hover:px-2">
-//           Verified
-//         </span>
-//       </span>
-//     </>
-//   )
-// }
-
-// export default BadgeWhitelisted
-
 import { FaUserCheck } from 'react-icons/fa'
 import { Privileges } from '../../constants/Privileges'
 
