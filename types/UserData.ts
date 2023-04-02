222058e93d75d95423fa3dc1a1d403937c82864c
def750 <def75023@gmail.com>
2023-04-02 18:30:00 +0000

    Profile stats (right)

--- types/UserData.ts
+++ types/UserData.ts
@@ -1,3 +1,5 @@
+import { IBeatmap } from './Map'
+
 export interface IPlayerData {
   status: boolean
   player: IPlayer
@@ -31,7 +33,7 @@ interface IPlayerInfo {
   userpage_content: string | null
 }
 
-interface IPlayerStatsModeData {
+export interface IPlayerStatsModeData {
   id: number
   tscore: number
   rscore: number
@@ -51,30 +53,6 @@ interface IPlayerStatsModeData {
   country_rank: number
   level: number
   level_progress: number
-}
-
-interface IBeatmap {
-  md5: string
-  id: number
-  server: string
-  set_id: number
-  artist: string
-  title: string
-  version: string
-  creator: string
-  last_update: string
-  total_length: number
-  max_combo: number
-  status: number
-  plays: number
-  passes: number
-  mode: number
-  bpm: number
-  cs: number
-  od: number
-  ar: number
-  hp: number
-  diff: number
 }
 
 export interface IPlayerStatus {
