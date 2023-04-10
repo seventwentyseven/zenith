396cfd3b237e750318229acf5f741ce99145f67b
dzifors <ggrzegrzolka11@gmail.com>
2023-04-10 16:43:48 +0000

    userpage: about me section

--- sections/user/AboutMe.tsx
+++ sections/user/AboutMe.tsx
@@ -1,8 +1,20 @@
-const AboutMeSection = () => {
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
+
+// TODO: Maybe try and find some library where you don't have to use dangerouslySetInnerHtml?
+const AboutMeSection = ({ aboutMe }: { aboutMe: string | null }) => {
+  if (aboutMe === null) return <></>
+
   return (
     <section className="flex flex-col w-full py-4 bg-hsl-10 bg-opacity-50 backdrop-blur-xl px-8 rounded-3xl -z-10 my-3">
       <div className="text-xl font-bold border-b-2 border-hsl-50 pb-0.5 w-min whitespace-nowrap ml-2">
         About Me
+      </div>
+      <div className="p-2">
+        <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+          {aboutMe.replaceAll('\n', '  \n\n')}
+        </ReactMarkdown>
       </div>
     </section>
   )
