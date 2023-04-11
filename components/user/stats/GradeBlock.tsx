ca10a865f8554eaed148534ce79d731a627ec511
dzifors <ggrzegrzolka11@gmail.com>
2023-04-11 12:21:09 +0000

    move grade images from public to assets folder

--- components/user/stats/GradeBlock.tsx
+++ components/user/stats/GradeBlock.tsx
@@ -1,11 +1,4 @@
-import Image from 'next/image'
-
-//? Grade images
-import gradeXH from '../../../public/grades/XH.png'
-import gradeX from '../../../public/grades/X.png'
-import gradeSH from '../../../public/grades/SH.png'
-import gradeS from '../../../public/grades/S.png'
-import gradeA from '../../../public/grades/A.png'
+import Grade from '../../Grade'
 
 interface IProps {
   xh_count: number
@@ -24,54 +17,24 @@ const GradeBlock = ({
 }: IProps) => {
   return (
     <div className="grid grid-cols-6 grid-rows-2 gap-y-3 w-11/12 mx-auto items-center justify-center mt-6 ml-5 pointer-events-none select-none">
-      <div className="flex flex-col items-center justify-center col-span-3 ml-[38px]">
-        <Image
-          src={gradeXH}
-          width={64}
-          height={64}
-          alt="Grade SS +Hidden"
-          className="shadow-xl z-10"
-        />
+      <div className="flex flex-col w-16 items-center justify-center col-span-3 ml-[38px]">
+        <Grade name="XH" />
         <span>{xh_count}</span>
       </div>
-      <div className="flex flex-col items-center justify-center col-span-3 mr-[38px]">
-        <Image
-          src={gradeX}
-          width={64}
-          height={64}
-          alt="Grade SS"
-          className="shadow-xl z-10"
-        />
+      <div className="flex flex-col w-16 items-center justify-center col-span-3 mr-[38px]">
+        <Grade name="X" />
         <span>{x_count}</span>
       </div>
-      <div className="flex flex-col items-center justify-center col-span-2">
-        <Image
-          src={gradeSH}
-          width={64}
-          height={64}
-          alt="Grade S +Hidden"
-          className="shadow-xl z-10"
-        />
+      <div className="flex flex-col w-16 items-center justify-center col-span-2">
+        <Grade name="SH" />
         <span>{sh_count}</span>
       </div>
-      <div className="flex flex-col items-center justify-center col-span-2">
-        <Image
-          src={gradeS}
-          width={64}
-          height={64}
-          alt="Grade S"
-          className="shadow-xl z-10"
-        />
+      <div className="flex flex-col w-16 items-center justify-center col-span-2">
+        <Grade name="S" />
         <span>{s_count}</span>
       </div>
-      <div className="flex flex-col items-center justify-center col-span-2">
-        <Image
-          src={gradeA}
-          width={64}
-          height={64}
-          alt="Grade A"
-          className="shadow-xl z-10"
-        />
+      <div className="flex flex-col w-16 items-center justify-center col-span-2">
+        <Grade name="A" />
         <span>{a_count}</span>
       </div>
     </div>
