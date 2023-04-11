c9d58bddf17f34aec6a488b64b50c3a0242ae60b
dzifors <ggrzegrzolka11@gmail.com>
2023-04-11 12:30:55 +0000

    delete home dashboard element (long overdue)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,57 @@
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Head from 'next/head'
-import { ReactNode } from 'react'
-import BackgroundImage from '../components/BackgroundImages'
+import BackgroundImage from '../components/BackgroundImage'
 import Layout from '../components/Layout'
+import QuickActions from '../components/QuickActions'
+import ArticleList from '../components/articles/ArticleList'
+import MostPlayed from '../components/sidebar/MostPlayed'
+import NewUser from '../components/sidebar/NewUser'
+import RecentRanked from '../components/sidebar/RecentRanked'
 import AboutUs from '../sections/home/AboutUs'
-import HomeDashboard from '../sections/home/Dashboard'
 import Hero from '../sections/home/Hero'
+import LoadingSection from '../sections/home/Loading'
 
 const Home: NextPage = () => {
   const { status } = useSession()
 
-  let pageContent: ReactNode
-
-  pageContent = <div>Loading...</div>
-
   if (status === 'authenticated')
-    pageContent = (
-      <>
+    return (
+      <Layout>
+        <Head>
+          <title>727</title>
+        </Head>
+
         <BackgroundImage opacity={0.6} blur />
-        <HomeDashboard />
-      </>
+
+        <div className="min-h-screen max-w-screen-xl w-full flex flex-col items-center mt-24 mx-auto">
+          <div className="w-full flex flex-col justify-between lg:flex-row h-auto">
+            {/* <Stats /> */}
+            <QuickActions />
+          </div>
+          <div className="w-full flex flex-col xl:flex-row justify-between h-auto">
+            <ArticleList />
+            <div className="w-[35%] h-fit flex flex-col justify-between rounded-lg p-4 bg-hsl-15 bg-opacity-20 backdrop-blur-xl shadow ml-2 mt-6 select-none border-white/20 border-px">
+              <RecentRanked />
+              <NewUser />
+              <MostPlayed />
+            </div>
+          </div>
+        </div>
+      </Layout>
     )
 
   if (status === 'unauthenticated')
-    pageContent = (
-      <>
+    return (
+      <Layout>
+        <Head>
+          <title>727</title>
+        </Head>
+
         <BackgroundImage />
         <Hero />
         <AboutUs />
-      </>
+      </Layout>
     )
 
   return (
@@ -38,7 +60,7 @@ const Home: NextPage = () => {
         <title>727</title>
       </Head>
 
-      {pageContent}
+      <LoadingSection />
     </Layout>
   )
 }
---
+++ sections/home/Loading.tsx
@@ -0,0 +1,12 @@
+import LoadingSpinner from '../../components/LoadingSpinner'
+
+const LoadingSection = () => {
+  return (
+    <section>
+      <LoadingSpinner color="hsl-50" />
+      <span>Loading</span>
+    </section>
+  )
+}
+
+export default LoadingSection
