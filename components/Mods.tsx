23fb30448b26a44b56ac31c2583d0568ebc44a7e
dzifors <ggrzegrzolka11@gmail.com>
2023-04-11 16:41:12 +0000

    update mods component so that doubletime doesn't show when nightcore is applied

--- components/Mods.tsx
+++ components/Mods.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
-import { ModImages } from '../constants/Mods'
+import { ModImages, Mods } from '../constants/Mods'
 
 interface IProps {
   mods: number
 }
 
 const ModsImageList = ({ mods }: IProps) => {
-  const modBadges = ModImages.filter(({ mod }) => (mods & mod) === mod)
+  let modBadges = ModImages.filter(({ mod }) => (mods & mod) === mod)
+
+  // Remove DoubleTime if Nightcore is applied
+  if ((mods & Mods.Nightcore) === Mods.Nightcore) {
+    modBadges = modBadges.filter(({ mod }) => mod !== Mods.DoubleTime)
+  }
+
   return (
     <div className="flex flex-row gap-2 max-w-3xl flex-wrap">
       {modBadges.map(({ mod, source }) => (
