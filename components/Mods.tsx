5cc248f743ec5fb501faecda665955c41f1a6c7f
dzifors <ggrzegrzolka11@gmail.com>
2023-04-15 20:52:46 +0200

    user/aboutme: make about me use up the whole space in the parent container

--- components/Mods.tsx
+++ components/Mods.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { ModImages, Mods } from '../constants/Mods'
+import * as Tooltip from '@radix-ui/react-tooltip'
 
 interface IProps {
   mods: number
@@ -16,9 +17,18 @@ const ModsImageList = ({ mods }: IProps) => {
   return (
     <div className="flex flex-row gap-2 max-w-3xl flex-wrap">
       {modBadges.map(({ mod, source }) => (
-        <div key={mod}>
-          <Image src={source} alt="modImage" height={35} />
-        </div>
+        <Tooltip.Provider key={`mod-${Mods[mod]}`} delayDuration={50}>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <div key={mod}>
+                <Image src={source} alt="modImage" height={35} />
+              </div>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content sideOffset={5}>{Mods[mod]}</Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
       ))}
     </div>
   )
