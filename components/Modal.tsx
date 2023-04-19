78552fa2185a78daac22d77c5e791b151d6854ae
dzifors <ggrzegrzolka11@gmail.com>
2023-04-19 09:25:35 +0200

    change ui library for modal element

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,50 +1,66 @@
-import { Dialog } from '@headlessui/react'
+import * as Dialog from '@radix-ui/react-dialog'
 import React, { ReactNode } from 'react'
+import { MdCancel } from 'react-icons/md'
 
 export const Modal = ({
-  children,
-  isOpen,
+  triggerElement,
   title,
   description,
-  handleCancel
+  children,
+  actionButton
 }: {
-  children: ReactNode
-  isOpen: boolean
+  triggerElement: ReactNode
   title: string
   description: string
-  handleCancel: () => void
+  children: ReactNode
+  actionButton?: ReactNode
 }) => {
   return (
-    <Dialog
-      open={isOpen}
-      onClose={handleCancel}
-      className="relative z-50 transition duration-300"
-    >
-      <div
-        className="fixed inset-0 bg-black/30 backdrop-blur-lg"
-        aria-hidden="true"
-      />
-
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="mx-auto max-w-screen-lg bg-hsl-15 p-8 rounded-xl flex flex-col gap-4">
-          <Dialog.Title className="font-bold text-2xl">{title}</Dialog.Title>
-          <Dialog.Description>{description}</Dialog.Description>
-
-          <div className="flex flex-col gap-4">{children}</div>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
-  )
-}
-
-export const ModalActions = ({ children }: { children: ReactNode }) => {
-  return <div className="flex gap-4 justify-end">{children}</div>
-}
-
-export const ModalForm = ({ children, ...rest }: { children: ReactNode }) => {
-  return (
-    <form {...rest} className="flex flex-col gap-4">
-      {children}
-    </form>
+    <Dialog.Root>
+      <Dialog.Trigger asChild>{triggerElement}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-hsl-5 bg-opacity-40 backdrop-blur-lg fixed inset-0" />
+        <Dialog.Content className="bg-hsl-15 rounded-md shadow shadow-hsl-5 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-screen-md max-h-[85vh] p-6 focus:outline-none">
+          <Dialog.Title className="m-0 font-medium text-sm">
+            {title}
+          </Dialog.Title>
+          <Dialog.Description className="mt-2 mb-5 text-hsl-90 text-sm">
+            {description}
+          </Dialog.Description>
+          {children}
+          {actionButton && (
+            <div
+              style={{
+                display: 'flex',
+                marginTop: 25,
+                justifyContent: 'flex-end'
+              }}
+            >
+              <Dialog.Close asChild>{actionButton}</Dialog.Close>
+            </div>
+          )}
+          <Dialog.Close asChild>
+            <button
+              style={{
+                fontFamily: 'inherit',
+                borderRadius: '100%',
+                height: '25px',
+                width: '25px',
+                display: 'inline-flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: 'black',
+                position: 'absolute',
+                top: '10px',
+                right: '10px'
+              }}
+              aria-label="Close"
+            >
+              <MdCancel />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
