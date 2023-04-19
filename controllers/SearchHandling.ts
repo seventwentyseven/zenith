ec2104d96ec1c08295f9c8ff3f3b57996b8a195e
dzifors <ggrzegrzolka11@gmail.com>
2023-04-19 09:25:09 +0200

    search: change handling for search

--- controllers/SearchHandling.ts
+++ controllers/SearchHandling.ts
@@ -1,11 +1,17 @@
+import { User } from 'next-auth'
 import config from '../config.json'
 
-const search = async (query: string, scope: string) => {
+const search = async (query: string, scope: string, token: User['token'], offset: number) => {
   const searchResponse = await fetch(
-    `${config.apiUrl}/v2/search?query=${query}&scope=${scope}`
+    `${config.apiUrl}/v2/search?query=${query}&scope=${scope}&offset=${offset}&limit=5`, {
+    headers: {
+      token: token
+    }
+  }
   )
   const response = await searchResponse.json()
   console.log(response)
+  return response
 }
 
 export { search }
