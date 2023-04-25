dc14b55c9c445606c0f687e4bff06e3530a71473
def750 <def75023@gmail.com>
2023-04-25 11:32:57 +0000

    Account standing component TODO: Auto generation

--- sections/user/AccountStanding.tsx
+++ sections/user/AccountStanding.tsx
@@ -1,8 +1,263 @@
+import { Table } from 'flowbite-react'
+
 const AccountStandingSection = () => {
   return (
     <section className="flex flex-col w-full py-4 bg-hsl-10 bg-opacity-50 backdrop-blur-xl px-8 rounded-3xl -z-10 my-3">
       <div className="text-xl font-bold border-b-2 border-hsl-50 pb-0.5 w-min whitespace-nowrap ml-2">
         Account Standing
+      </div>
+      {/* Table with following fields 
+        Moderator: flag + username
+        Action: Bullet badge with action taken
+        Reason: String (Add duration in case of timed actions)
+        Date: Date
+        Additionaly, if Head Admin or owner, On right edit button
+
+        // Data for normal users
+        Action
+        Reason
+        Date
+      */}
+      <div className="relative overflow-x-auto mt-1">
+        <table className="w-full text-sm text-left ">
+          <thead className="text-xs text-hsl-80 uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-3">
+                Admin
+              </th>
+              <th scope="col" className="px-7 py-3">
+                Action
+              </th>
+              <th scope="col" className="px-3 py-3">
+                Reason
+              </th>
+              <th scope="col" className="px-3 py-3">
+                Date
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-base">
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/PL.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    def750
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                <div className="w-28 py-0.5 inline-flex bg-red-500 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-red-900 uppercase pl-2">
+                    TOURN. BAN
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                1 year, cheating
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">1 day ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/PL.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    def750
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                <div className="w-28 py-0.5 inline-flex bg-lime-500 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-green-900 uppercase pl-2">
+                    UNRESTRICT
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                2nd Chance
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">1 day ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/PT.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    Dolphin_
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                {/* bullet badge */}
+                <div className="w-28 py-0.5 inline-flex bg-red-500 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-rose-900 uppercase pl-2">
+                    RESTRICT
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                Cheating (Blatant)
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">4 months ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/JP.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    minatoaqua2
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                {/* bullet badge */}
+                <div className="w-28 py-0.5 inline-flex bg-yellow-300 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-yellow-900 uppercase pl-2">
+                    SILENCE
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                Harassment (30 days) (Expired)
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">5 months ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/PL.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    dzifors
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                {/* bullet badge */}
+                <div className="w-28 py-0.5 inline-flex bg-blue-500 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-teal-900 uppercase pl-2">
+                    NOTE
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                Previous Multiaccounts + Ban Evasion on Userids: 334, 557, 1075,
+                2230, 3340. All restricted
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">1 year ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                ></a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/GB.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    Thief
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                {/* bullet badge */}
+                <div className="w-28 py-0.5 inline-flex bg-lime-500 rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-green-900 uppercase pl-2">
+                    UNSILENCE
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                Nah
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">1 year ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   )
