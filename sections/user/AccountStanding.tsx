44c6aced3fac10e997a15725cfb7bf5f51b2acb6
def750 <def75023@gmail.com>
2023-04-27 12:32:37 +0000

    Demotion

--- sections/user/AccountStanding.tsx
+++ sections/user/AccountStanding.tsx
@@ -1,6 +1,25 @@
 import { Table } from 'flowbite-react'
+import { ISODateString } from 'next-auth'
 
+interface IAccountStandingEntry {
+  id: number
+  msg: string
+  date: ISODateString
+  // rest is optional
+  from_id?: number
+  from_name?: string
+  from_country?: string
+  to_id?: number
+  to_name?: string
+  to_country?: string
+}
+
+// depending on requester privileges show this block as
+// normal: Account Standing
+// staff: Account History
+// head admin: Account History + Edit button
 const AccountStandingSection = () => {
+  // Fetch
   return (
     <section className="flex flex-col w-full py-4 bg-hsl-10 bg-opacity-50 backdrop-blur-xl px-8 rounded-3xl -z-10 my-3">
       <div className="text-xl font-bold border-b-2 border-hsl-50 pb-0.5 w-min whitespace-nowrap ml-2">
@@ -247,6 +266,43 @@ const AccountStandingSection = () => {
                 Nah
               </th>
               <td className="px-3 mb-2 whitespace-nowrap">1 year ago</td>
+              <td className="pl-3 mb-2">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="pl-2 pr-4 mb-2 mr-auto">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={`https://seventwentyseven.xyz/static/images/flags/BR.png`}
+                    alt={`Poland`}
+                    className="h-5 w-auto"
+                  />
+                  <a
+                    href="#"
+                    className="font-medium text-hsl-90 hover:text-hsl-80 hover:underline transition-colors duration-100"
+                  >
+                    Min60
+                  </a>
+                </div>
+              </td>
+              <td className="px-6 mb-2">
+                {/* bullet badge */}
+                <div className="w-28 py-0.5 inline-flex bg-[#05041d] rounded-full">
+                  <span className="text-sm leading-5 font-bold font-nunito text-yellow-300 uppercase pl-2">
+                    DEMOTION
+                  </span>
+                </div>
+              </td>
+              <th scope="row" className="px-3 mb-2 font-medium ">
+                Lying, banning people for no reason, being a general asshole
+              </th>
+              <td className="px-3 mb-2 whitespace-nowrap">2 years ago</td>
               <td className="pl-3 mb-2">
                 <a
                   href="#"
