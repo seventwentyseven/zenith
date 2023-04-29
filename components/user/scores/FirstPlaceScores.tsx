d0e140d086eb6a1c49ab5a12e4bb081f96956df2
dzifors <ggrzegrzolka11@gmail.com>
2023-04-29 14:11:16 +0000

    scores: add placeholder first place score

--- components/user/scores/FirstPlaceScores.tsx
+++ components/user/scores/FirstPlaceScores.tsx
@@ -11,7 +11,51 @@ const FirstPlaceScores = ({
   userid: number
   gamemode: number
 }) => {
-  const [scores, setScores] = useState<IScore[]>([])
+  const [scores, setScores] = useState<IScore[]>([
+    {
+      acc: 100,
+      beatmap: {
+        ar: 10,
+        artist: 'dzifors',
+        bpm: 300,
+        creator: 'dzifors',
+        cs: 3,
+        diff: 3,
+        hp: 3,
+        id: 3123,
+        last_update: 'asdas',
+        max_combo: 123,
+        md5: '123123',
+        mode: 3,
+        od: 123,
+        passes: 123,
+        plays: 2,
+        server: 'osu!',
+        set_id: 123123,
+        status: 2,
+        title: 'dzifors',
+        total_length: 3123,
+        version: 'jajco kurwa'
+      },
+      grade: 'A',
+      id: 3,
+      max_combo: 3123,
+      mode: 3,
+      mods: 123,
+      n100: 2,
+      n300: 3,
+      n50: 123,
+      ngeki: 300,
+      nkatu: 123,
+      nmiss: 12,
+      perfect: 1,
+      play_time: 'asda',
+      pp: 123,
+      score: 3123123,
+      status: 2,
+      time_elapsed: 12398176
+    }
+  ])
   const [loading, setLoading] = useState<boolean>(true)
   const [oldGamemode, setOldGamemode] = useState<number>(gamemode)
   const [offset, setOffset] = useState<number>(0)
@@ -20,46 +64,46 @@ const FirstPlaceScores = ({
     if (offset < 95) setOffset(offset + 5)
   }
 
-  const fetchFirstPlaceScores = async (fetchingOffset: number) => {
-    if (gamemode !== oldGamemode) {
-      setLoading(true)
-      setOldGamemode(gamemode)
-      setScores([])
-      await setOffset(0)
-      const newScores = await fetchScores(
-        'first',
-        userid,
-        gamemode,
-        fetchingOffset
-      )
-      setScores(current => [...current, ...newScores])
-      setLoading(false)
-    } else {
-      setLoading(true)
-      const newScores = await fetchScores(
-        'first',
-        userid,
-        gamemode,
-        fetchingOffset
-      )
-      setScores(current => [...current, ...newScores])
-      setLoading(false)
-    }
-  }
+  // const fetchFirstPlaceScores = async (fetchingOffset: number) => {
+  //   if (gamemode !== oldGamemode) {
+  //     setLoading(true)
+  //     setOldGamemode(gamemode)
+  //     setScores([])
+  //     await setOffset(0)
+  //     const newScores = await fetchScores(
+  //       'first',
+  //       userid,
+  //       gamemode,
+  //       fetchingOffset
+  //     )
+  //     setScores(current => [...current, ...newScores])
+  //     setLoading(false)
+  //   } else {
+  //     setLoading(true)
+  //     const newScores = await fetchScores(
+  //       'first',
+  //       userid,
+  //       gamemode,
+  //       fetchingOffset
+  //     )
+  //     setScores(current => [...current, ...newScores])
+  //     setLoading(false)
+  //   }
+  // }
 
-  const fetchFirstPlaceScoresFirstTime = async () => {
-    setLoading(true)
-    const newScores = await fetchScores('first', userid, gamemode, 0)
-    setScores(newScores)
-    setLoading(false)
-  }
+  // const fetchFirstPlaceScoresFirstTime = async () => {
+  //   setLoading(true)
+  //   const newScores = await fetchScores('first', userid, gamemode, 0)
+  //   setScores(newScores)
+  //   setLoading(false)
+  // }
 
-  useEffect(() => {
-    setScores([])
-    setOffset(0)
-    setOldGamemode(gamemode)
-    fetchFirstPlaceScoresFirstTime()
-  }, [gamemode, userid])
+  // useEffect(() => {
+  //   setScores([])
+  //   setOffset(0)
+  //   setOldGamemode(gamemode)
+  //   fetchFirstPlaceScoresFirstTime()
+  // }, [gamemode, userid])
 
   console.log(`scores.length: ${scores?.length}`)
   return (
