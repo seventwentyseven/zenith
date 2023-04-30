b59c414626ab3bc101855dd98ec39afbae6464f4
dzifors <dzifors>
2023-04-30 17:24:05 +0200

    scores: fix blue line going out of scores section

--- sections/user/Scores.tsx
+++ sections/user/Scores.tsx
@@ -11,7 +11,7 @@ const ScoresSection = ({
 }) => {
   return (
     <section className="flex flex-col w-full py-4 bg-hsl-10 bg-opacity-50 backdrop-blur-xl px-8 rounded-3xl -z-10 my-3">
-      <div className="text-xl font-bold border-b-2 border-hsl-50 w-full flex flex-row items-center whitespace-nowrap ml-2">
+      <div className="text-xl font-bold border-b-2 border-hsl-50 w-[calc(100%-1rem)] flex flex-row items-center whitespace-nowrap mx-2">
         Scores
         <div className="-mb-1 ml-5">
           <ul
