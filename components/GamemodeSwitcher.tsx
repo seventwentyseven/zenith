f0a4671248652c2522f72d03b367117988f07f65
dzifors <ggrzegrzolka11@gmail.com>
2023-05-02 11:57:07 +0200

    components: remove flowbite from gamemode switcher

--- components/GamemodeSwitcher.tsx
+++ components/GamemodeSwitcher.tsx
@@ -1,6 +1,13 @@
 import classNames from 'classnames'
 import { initPopovers } from 'flowbite'
 import { SetStateAction, useEffect, useState } from 'react'
+import {
+  HoverCard,
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardPortal,
+  HoverCardTrigger
+} from './ui/HoverCard'
 
 interface IProps {
   gamemode: number
@@ -28,7 +35,6 @@ const ModeStateMap: IModeStateMap = {
   6: { std: false, taiko: false, catch: true, mania: false },
   8: { std: true, taiko: false, catch: false, mania: false }
 }
-
 const GamemodeSwitcher = ({ gamemode, setGamemode }: IProps) => {
   const [stdSelected, setStdSelected] = useState<boolean>(false)
   const [taikoSelected, setTaikoSelected] = useState<boolean>(false)
@@ -49,195 +55,207 @@ const GamemodeSwitcher = ({ gamemode, setGamemode }: IProps) => {
 
   return (
     <div className="dark flex flex-row gap-2">
-      <button
-        data-popover-target="popover-std"
-        data-popover-placement="bottom"
-        type="button"
-        className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
-          stdSelected
-            ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
-            : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
-        }`}
-        onClick={() => setGamemode(0)}
-      >
-        <i className="mode-icon osu"></i>
-      </button>
-
-      <button
-        data-popover-target="popover-taiko"
-        data-popover-placement="bottom"
-        type="button"
-        className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
-          taikoSelected
-            ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
-            : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
-        }`}
-        onClick={() => setGamemode(1)}
-      >
-        <i className="mode-icon taiko"></i>
-      </button>
-      <button
-        data-popover-target="popover-catch"
-        data-popover-placement="bottom"
-        type="button"
-        className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
-          catchSelected
-            ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
-            : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
-        }`}
-        onClick={() => setGamemode(2)}
-      >
-        <i className="mode-icon catch"></i>
-      </button>
-      <button
-        data-popover-target="popover-mania"
-        data-popover-placement="bottom"
-        type="button"
-        className={`px-3 py-2 text-white rounded duration-300  ${
-          maniaSelected
-            ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
-            : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
-        }`}
-        onClick={() => setGamemode(3)}
-      >
-        <i className="mode-icon mania"></i>
-      </button>
-
-      <div
-        data-popover
-        id="popover-std"
-        role="tooltip"
-        className="absolute z-10 invisible inline-block w-48 text-sm font-light transition-opacity duration-300 rounded-lg shadow-sm shadow-hsl-5 bg-hsl-15 bg-opacity-80 opacity-0"
-      >
-        <div className="px-3 py-2 ">
-          <h3 className="font-semibold text-gray-200 text-center">osu!</h3>
-        </div>
-        <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+      <HoverCard>
+        <HoverCardTrigger asChild>
           <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 0 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
+            type="button"
+            className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
+              stdSelected
+                ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
+                : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
+            }`}
             onClick={() => setGamemode(0)}
           >
-            vanilla
+            <i className="mode-icon osu"></i>
           </button>
-          <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 4 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
-            onClick={() => setGamemode(4)}
+        </HoverCardTrigger>
+        <HoverCardPortal>
+          <HoverCardContent
+            side="bottom"
+            className="inline-block w-48 bg-hsl-15 bg-opacity-80 shadow-sm shadow-hsl-5 rounded-lg font-light"
           >
-            relax
-          </button>
-          <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 8 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
-            onClick={() => setGamemode(8)}
-          >
-            autopilot
-          </button>
-        </div>
-        <div data-popper-arrow></div>
-      </div>
+            <div className="px-3 py-2">
+              <h3 className="font-semibold text-gray-200 text-center">osu!</h3>
+            </div>
+            <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 0 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(0)}
+              >
+                vanilla
+              </button>
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 4 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(4)}
+              >
+                relax
+              </button>
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 8 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(8)}
+              >
+                autopilot
+              </button>
+            </div>
+            <HoverCardArrow className="fill-hsl-15 opacity-80" />
+          </HoverCardContent>
+        </HoverCardPortal>
+      </HoverCard>
 
-      <div
-        data-popover
-        id="popover-taiko"
-        role="tooltip"
-        className="absolute z-10 invisible inline-block w-48 text-sm font-light transition-opacity duration-300 rounded-lg shadow-sm shadow-hsl-5 bg-hsl-15 bg-opacity-80 opacity-0"
-      >
-        <div className="px-3 py-2 ">
-          <h3 className="font-semibold text-gray-200 text-center">osu!taiko</h3>
-        </div>
-        <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+      <HoverCard>
+        <HoverCardTrigger asChild>
           <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 1 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
+            type="button"
+            className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
+              taikoSelected
+                ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
+                : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
+            }`}
             onClick={() => setGamemode(1)}
           >
-            vanilla
+            <i className="mode-icon taiko" />
           </button>
-          <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 5 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
-            onClick={() => setGamemode(5)}
+        </HoverCardTrigger>
+        <HoverCardPortal>
+          <HoverCardContent
+            side="bottom"
+            className="inline-block w-48 bg-hsl-15 bg-opacity-80 shadow-sm shadow-hsl-5 rounded-lg font-light"
           >
-            relax
-          </button>
-        </div>
-        <div data-popper-arrow></div>
-      </div>
+            <div className="px-3 py-2 ">
+              <h3 className="font-semibold text-gray-200 text-center">
+                osu!taiko
+              </h3>
+            </div>
+            <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 1 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(1)}
+              >
+                vanilla
+              </button>
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 5 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(5)}
+              >
+                relax
+              </button>
+            </div>
+            <HoverCardArrow className="fill-hsl-15 opacity-80" />
+          </HoverCardContent>
+        </HoverCardPortal>
+      </HoverCard>
 
-      <div
-        data-popover
-        id="popover-catch"
-        role="tooltip"
-        className="absolute z-10 invisible inline-block w-48 text-sm font-light transition-opacity duration-300 rounded-lg shadow-sm shadow-hsl-5 bg-hsl-15 bg-opacity-80 opacity-0"
-      >
-        <div className="px-3 py-2 ">
-          <h3 className="font-semibold text-gray-200 text-center">osu!catch</h3>
-        </div>
-        <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+      <HoverCard>
+        <HoverCardTrigger asChild>
           <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 2 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
+            type="button"
+            className={`px-3 py-2 text-white rounded duration-300 hover:bg-hsl-10 hover:bg-opacity-80 ${
+              catchSelected
+                ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
+                : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
+            }`}
             onClick={() => setGamemode(2)}
           >
-            vanilla
+            <i className="mode-icon catch"></i>
           </button>
-          <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 6 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
-            onClick={() => setGamemode(6)}
+        </HoverCardTrigger>
+        <HoverCardPortal>
+          <HoverCardContent
+            side="bottom"
+            className="inline-block w-48 bg-hsl-15 bg-opacity-80 shadow-sm shadow-hsl-5 rounded-lg font-light"
           >
-            relax
-          </button>
-        </div>
-        <div data-popper-arrow></div>
-      </div>
+            <div className="px-3 py-2 ">
+              <h3 className="font-semibold text-gray-200 text-center">
+                osu!catch
+              </h3>
+            </div>
+            <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 2 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(2)}
+              >
+                vanilla
+              </button>
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 6 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(6)}
+              >
+                relax
+              </button>
+            </div>
+            <HoverCardArrow className="fill-hsl-15 opacity-80" />
+          </HoverCardContent>
+        </HoverCardPortal>
+      </HoverCard>
 
-      <div
-        data-popover
-        id="popover-mania"
-        role="tooltip"
-        className="absolute z-10 invisible inline-block w-48 text-sm font-light transition-opacity duration-300 rounded-lg shadow-sm shadow-hsl-5 bg-hsl-15 bg-opacity-80 opacity-0"
-      >
-        <div className="px-3 py-2 ">
-          <h3 className="font-semibold text-gray-200 text-center">osu!mania</h3>
-        </div>
-        <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+      <HoverCard>
+        <HoverCardTrigger asChild>
           <button
-            className={classNames(
-              'btn btn-md',
-              gamemode === 3 ? 'btn-secondary' : 'btn-primary',
-              'w-full'
-            )}
+            type="button"
+            className={`px-3 py-2 text-white rounded duration-300  ${
+              maniaSelected
+                ? 'bg-hsl-55 bg-opacity-80 hover:bg-hsl-75 hover:bg-opacity-60'
+                : 'bg-hsl-15 bg-opacity-60 hover:bg-hsl-10 hover:bg-opacity-80'
+            }`}
             onClick={() => setGamemode(3)}
           >
-            Vanilla
+            <i className="mode-icon mania" />
           </button>
-        </div>
-        <div data-popper-arrow></div>
-      </div>
+        </HoverCardTrigger>
+        <HoverCardPortal>
+          <HoverCardContent
+            side="bottom"
+            className="inline-block w-48 bg-hsl-15 bg-opacity-80 shadow-sm shadow-hsl-5 rounded-lg font-light"
+          >
+            <div className="px-3 py-2 ">
+              <h3 className="font-semibold text-gray-200 text-center">
+                osu!mania
+              </h3>
+            </div>
+            <div className="px-3 pb-2 flex flex-col items-center justify-between gap-2">
+              <button
+                className={classNames(
+                  'btn btn-md',
+                  gamemode === 3 ? 'btn-secondary' : 'btn-primary',
+                  'w-full'
+                )}
+                onClick={() => setGamemode(3)}
+              >
+                Vanilla
+              </button>
+            </div>
+            <HoverCardArrow className="fill-hsl-15 opacity-80" />
+          </HoverCardContent>
+        </HoverCardPortal>
+      </HoverCard>
     </div>
   )
 }
