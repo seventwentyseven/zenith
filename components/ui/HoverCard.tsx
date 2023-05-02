81175f8f31b9765ff87f75d5202f7b1ec9fccc15
dzifors <ggrzegrzolka11@gmail.com>
2023-05-02 11:56:27 +0200

    ui: update hover card component

--- components/ui/HoverCard.tsx
+++ components/ui/HoverCard.tsx
@@ -7,21 +7,41 @@ const HoverCard = HoverCardPrimitive.Root
 
 const HoverCardTrigger = HoverCardPrimitive.Trigger
 
+const HoverCardPortal = HoverCardPrimitive.Portal
+
+const HoverCardArrow = HoverCardPrimitive.Arrow
+
 const HoverCardContent = React.forwardRef<
   React.ElementRef<typeof HoverCardPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
-  <HoverCardPrimitive.Content
-    ref={ref}
-    align={align}
-    sideOffset={sideOffset}
-    className={cn(
-      'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none animate-in zoom-in-90',
-      className
-    )}
-    {...props}
-  />
-))
+>(
+  (
+    {
+      className,
+      // align = 'center',
+      // sideOffset = 4,
+      ...props
+    },
+    ref
+  ) => (
+    <HoverCardPrimitive.Content
+      ref={ref}
+      // align={align}
+      // sideOffset={sideOffset}
+      className={cn(
+        // 'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none animate-in zoom-in-90',
+        className
+      )}
+      {...props}
+    />
+  )
+)
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
 
-export { HoverCard, HoverCardTrigger, HoverCardContent }
+export {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+  HoverCardArrow,
+  HoverCardPortal
+}
