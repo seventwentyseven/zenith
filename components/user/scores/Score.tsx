9a9c6c0155a0c8fa792ef6a8e41adff0ebb416df
dzifors <ggrzegrzolka11@gmail.com>
2023-05-02 11:57:35 +0200

    scores: update score component to use less hardcoded pixel values

--- components/user/scores/Score.tsx
+++ components/user/scores/Score.tsx
@@ -9,12 +9,12 @@ const Score = ({ score }: { score: IScore }) => {
   return (
     <div className="w-full h-24 flex group">
       <div
-        className="bg-hsl-10 flex flex-row items-center justify-between px-4 rounded-xl min-w-full h-full transition duration-300 bg-center bg-cover group-hover:-translate-x-[4%]"
+        className="bg-hsl-10 flex flex-row items-center justify-between px-4 rounded-xl min-w-full h-full transition duration-300 bg-center bg-cover group-hover:-translate-x-12"
         style={{
           backgroundImage: `linear-gradient(180deg, hsla(230, 10%, 10%, 0.7), hsla(230, 10%, 10%, 0.7)), url("https://assets.ppy.sh/beatmaps/${score.beatmap.set_id}/covers/cover.jpg")`
         }}
       >
-        <div className="flex flex-col gap-2 text-hsl-90 group-hover:translate-x-12 transition duration-300">
+        <div className="flex flex-col gap-2 text-hsl-90">
           <Link href={`/beatmaps/${score.beatmap.id}`} className="text-xl">
             {score.beatmap.artist} - {score.beatmap.title}
           </Link>
@@ -33,14 +33,14 @@ const Score = ({ score }: { score: IScore }) => {
             <span className="text-2xl font-bold">{Math.round(score.pp)}pp</span>
             <span className="text-white/70">{score.acc.toFixed(2)}%</span>
           </div>
-          <span className="text-5xl w-[68px] h-full">
+          <span className="text-5xl w-16 h-full">
             <Grade name={score.grade} />
           </span>
         </div>
       </div>
       <Link
         href={`/scores/${score.id}`}
-        className="w-[4%] h-full opacity-0 scale-0 flex translate-x-0 group-hover:scale-100 transition group-hover:opacity-100 group-hover:-translate-x-7 duration-300 items-center justify-center"
+        className="w-12 h-full opacity-0 scale-0 flex translate-x-0 group-hover:scale-100 transition group-hover:opacity-100 group-hover:-translate-x-7 duration-300 items-center justify-center"
       >
         <FaBars />
       </Link>
