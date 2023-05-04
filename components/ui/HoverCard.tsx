a64aadf83fbec884b42f554e3b43b833710c9c5c
dzifors <ggrzegrzolka11@gmail.com>
2023-05-04 22:16:24 +0200

    ui: modify hover card component

--- components/ui/HoverCard.tsx
+++ components/ui/HoverCard.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react'
 import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
-
-import { cn } from '../../utlis/ClassNames'
+import * as React from 'react'
 
 const HoverCard = HoverCardPrimitive.Root
 
@@ -10,32 +8,18 @@ const HoverCardTrigger = HoverCardPrimitive.Trigger
 const HoverCardPortal = HoverCardPrimitive.Portal
 
 const HoverCardArrow = HoverCardPrimitive.Arrow
-
 const HoverCardContent = React.forwardRef<
   React.ElementRef<typeof HoverCardPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
->(
-  (
-    {
-      className,
-      // align = 'center',
-      // sideOffset = 4,
-      ...props
-    },
-    ref
-  ) => (
-    <HoverCardPrimitive.Content
-      ref={ref}
-      // align={align}
-      // sideOffset={sideOffset}
-      className={cn(
-        // 'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none animate-in zoom-in-90',
-        className
-      )}
-      {...props}
-    />
-  )
-)
+>(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+  <HoverCardPrimitive.Content
+    ref={ref}
+    align={align}
+    sideOffset={sideOffset}
+    className={className}
+    {...props}
+  />
+))
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
 
 export {
