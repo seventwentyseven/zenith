bd9b198b6273518b602c7adc746e3ea556f0344e
dzifors <ggrzegrzolka11@gmail.com>
2023-06-07 22:06:13 +0000

    signin page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,20 +1,25 @@
 import Link from 'next/link'
+import BackgroundImage from '~/components/background-image'
+import Layout from '~/components/layout'
 import SigninForm from '~/sections/auth/signin-form'
 
 const SigninPage = () => {
   return (
-    <section className="w-full max-w-screen-sm transition duration-200">
-      <div className="mb-8 flex flex-row items-center justify-between rounded-lg bg-hsl-35-15 bg-opacity-50 px-6 py-4 backdrop-blur-xl">
-        <span className="text-3xl font-semibold text-white">Sign in</span>
-        <Link
-          href="/auth/signup"
-          className="text-blue-300 duration-150 hover:text-blue-400 focus:text-blue-400 active:text-blue-500"
-        >
-          Don&apos;t have an account?
-        </Link>
-      </div>
-      <SigninForm />
-    </section>
+    <Layout>
+      <BackgroundImage blur />
+      <section className="w-full max-w-screen-sm transition duration-200">
+        <div className="mb-8 flex flex-row items-center justify-between rounded-lg bg-hsl-35-15 bg-opacity-50 px-6 py-4 backdrop-blur-xl">
+          <span className="text-3xl font-semibold text-white">Sign in</span>
+          <Link
+            href="/auth/signup"
+            className="text-blue-300 duration-150 hover:text-blue-400 focus:text-blue-400 active:text-blue-500"
+          >
+            Don&apos;t have an account?
+          </Link>
+        </div>
+        <SigninForm />
+      </section>
+    </Layout>
   )
 }
 
