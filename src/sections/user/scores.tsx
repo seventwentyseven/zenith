684abd8cdbd700803c9162839218a9d93aabdfff
dzifors <ggrzegrzolka11@gmail.com>
2023-06-07 22:07:34 +0000

    scores section

---
+++ src/sections/user/scores.tsx
@@ -0,0 +1,12 @@
+const Scores = () => {
+  return (
+    <div className="my-3 flex w-full flex-col rounded-3xl bg-hsl-35-10 bg-opacity-80 px-8 py-4">
+      <div className="mx-2 flex w-fit items-center whitespace-nowrap border-b-2 border-hsl-50 text-xl font-bold">
+        Scores
+      </div>
+      <span className="p-2">TODO</span>
+    </div>
+  )
+}
+
+export default Scores
